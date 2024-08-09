4b57b7fca751bc6533e08b315e990a8c2ef425dd
rizzamaulaa <mauludia_maula_33rpl@student.smktelkom-mlg.sch.id>
2024-08-09 22:14:23 +0700

    Add files via upload

---
+++ Bukuuu.java
@@ -0,0 +1,39 @@
+public class Bukuuu {
+    public static void main(String[] args) {
+        //objek dgn constructor parameter
+        Buku Rizza = new Buku(123, "cinta", "kamu", 2020);
+        System.out.println("Rizza meminjam buku : ");
+        System.out.println("id" + Rizza.id);
+        System.out.println("judul" + Rizza.judul);
+        System.out.println("pengarang" + Rizza.pengarang);
+        System.out.println("tahunTerbit" + Rizza.tahunTerbit);
+
+      Buku moza = new Buku(234, "langit", "dia", 2050);
+        System.out.println("moza meminjam buku : ");
+        System.out.println("id" + moza.id);
+        System.out.println("judul" + moza.judul);
+        System.out.println("pengarang" + moza.pengarang);
+        System.out.println("tahunTerbit" + moza.tahunTerbit);
+
+      Buku Azka = new Buku(345, "laut", "kalian", 2002);
+        System.out.println("Azka meminjam buku : ");
+        System.out.println("id" + Azka.id);
+        System.out.println("judul" + Azka.judul);
+        System.out.println("pengarang" + Azka.pengarang);
+        System.out.println("tahunTerbit" + Azka.tahunTerbit);
+        
+      Buku Fadhil = new Buku(567, "angin", "dirimu", 1998);
+        System.out.println("Fadhil meminjam buku : ");
+        System.out.println("id" + Fadhil.id);
+        System.out.println("judul" + Fadhil.judul);
+        System.out.println("pengarang" + Fadhil.pengarang);
+        System.out.println("tahunTerbit" + Fadhil.tahunTerbit);
+
+      Buku Atha = new Buku(890, "dia", "orang", 2080);
+        System.out.println("Atha meminjam buku : ");
+        System.out.println("id" + Atha.id);
+        System.out.println("judul" + Atha.judul);
+        System.out.println("pengarang" + Atha.pengarang);
+        System.out.println("tahunTerbit" + Atha.tahunTerbit);
+    }
+}
